6a6a890ea5b397ba513229edfd729a82b2e8b7dc
v_haovhuang <v_haovhuang@tencent.com>
2023-02-02 10:52:56 +0800

    首页dome

---
+++ subpkg/goods_detail/goods_detail.vue
@@ -0,0 +1,22 @@
+<template>
+    <view>
+        轮播详情
+    </view>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                
+            }
+        },
+        methods: {
+            
+        }
+    }
+</script>
+
+<style>
+
+</style>
