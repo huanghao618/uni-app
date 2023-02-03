28d1566062a150c134f9f045a256c40dea95c276
v_haovhuang <v_haovhuang@tencent.com>
2023-02-03 14:27:52 +0800

    购物车done

--- components/my-settle/my-settle.vue
+++ components/my-settle/my-settle.vue
@@ -13,13 +13,14 @@
             </view>
 
             <!-- 结算按钮 -->
-            <view class="btn-settle">结算({{checkedCount}})</view>
+            <view class="btn-settle" @click="settlement">结算({{checkedCount}})</view>
         </view>
     </view>
 </template>
 
 <script>
     import {
+        mapState,
         mapGetters,
         mapMutations
     } from 'vuex'
@@ -27,6 +28,9 @@
         name: "my-settle",
         computed: {
             ...mapGetters('m_cart', ['total', 'checkedCount', 'checkedGoodsAmount']),
+            ...mapGetters('m_user', ['addstr']),
+            ...mapState('m_user', ['token']),
+            ...mapState('m_cart', ['cart']),
             // 2. 是否全选
             isFullCheck() {
                 return this.total === this.checkedCount
@@ -36,15 +40,81 @@
             // 2. 使用 mapMutations 辅助函数，把 m_cart 模块提供的 updateAllGoodsState 方法映射到当前组件中使用
             ...mapMutations('m_cart', ['updateAllGoodsState']),
             changeAllState() {
-                console.log(1)
                 // 修改购物车中所有商品的选中状态
                 // !this.isFullCheck 表示：当前全选按钮的状态取反之后，就是最新的勾选状态
                 this.updateAllGoodsState(!this.isFullCheck)
+            },
+            // 点击了结算按钮
+            settlement() {
+                // 1. 先判断是否勾选了要结算的商品
+                if (!this.checkedCount) return uni.$showMsg('请选择要结算的商品！')
+
+                // 2. 再判断用户是否选择了收货地址
+                // if (!this.addstr) return uni.$showMsg('请选择收货地址！')
+
+                // 3. 最后判断用户是否登录了
+                if (!this.token) return uni.$showMsg('请先登录！')
+                // 4. 实现微信支付功能
+                this.payOrder()
             }
         },
         data() {
             return {
+                // 微信支付
+                async payOrder() {
+                    // 1. 创建订单
+                    // 1.1 组织订单的信息对象
+                    const orderInfo = {
+                        // 开发期间，注释掉真实的订单价格，
+                        // order_price: this.checkedGoodsAmount,
+                        // 写死订单总价为 1 分钱
+                        order_price: 0.01,
+                        consignee_addr: this.addstr,
+                        goods: this.cart.filter(x => x.goods_state).map(x => ({
+                            goods_id: x.goods_id,
+                            goods_number: x.goods_count,
+                            goods_price: x.goods_price
+                        }))
+                    }
+                    // 1.2 发起请求创建订单
+                    const {
+                        data: res
+                    } = await uni.$http.post('/api/public/v1/my/orders/create', orderInfo)
+                    // if (res.meta.status !== 200) return uni.$showMsg('创建订单失败！')
+                    // 1.3 得到服务器响应的“订单编号”
+                    // const orderNumber = res.message.order_number
 
+                    // 2. 订单预支付
+                    // 2.1 发起请求获取订单的支付信息
+                    const {
+                        data: res2
+                    } = await uni.$http.post('/api/public/v1/my/orders/req_unifiedorder', {
+                        order_number: 'orderNumber'
+                    })
+                    // 2.2 预付订单生成失败
+                    if (res2.meta.status !== 200) return uni.$showError('预付订单生成失败！')
+                    // 2.3 得到订单支付相关的必要参数
+                    const payInfo = res2.message.pay
+
+                    // 3. 发起微信支付
+                    // 3.1 调用 uni.requestPayment() 发起微信支付
+                    const [err, succ] = await uni.requestPayment(payInfo)
+                    // 3.2 未完成支付
+                    if (err) return uni.$showMsg('订单未支付！')
+                    // 3.3 完成了支付，进一步查询支付的结果
+                    const {
+                        data: res3
+                    } = await uni.$http.post('/api/public/v1/my/orders/chkOrder', {
+                        order_number: orderNumber
+                    })
+                    // 3.4 检测到订单未支付
+                    if (res3.meta.status !== 200) return uni.$showMsg('订单未支付！')
+                    // 3.5 检测到订单支付完成
+                    uni.showToast({
+                        title: '支付完成！',
+                        icon: 'success'
+                    })
+                }
             };
         }
     }
